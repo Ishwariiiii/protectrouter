d41a116a15106be87fb7b345a9954de93edd90b1
Ishwariiiii <ishwarikanathe@gmail.com>
2024-09-17 13:11:04 -0700

    17-09-24

---
+++ forgotresetpassword/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Add from './components/Add'
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <p>rretyty</p>
+      <Add />
+    </div>
+
+  )
+}
+
+export default App
---
+++ forgotresetpassword/src/components/Add.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Add() {
+    return (
+        <div>add component
+            <p>helroriuifgjfdg</p>
+        </div>
+
+    )
+}
+
+export default Add
